1371b5fc317b2e08c75c8de8315aa02c8bac6b37
renataa <renataxdalej@gmail.com>
2024-07-24 18:21:58 -0500

    nuevo

---
+++ PROGRAMACIÓN AVANZADA/web/js-aux/diccionario1.js
@@ -0,0 +1,22 @@
+var csvFilePath = 'archivos/csv/diccionario1.csv';
+Papa.parse(csvFilePath, {
+    download: true,
+    complete: function (results) {
+        var tableData = results.data;
+        var tableHtml = '';
+
+        for (var i = 0; i < tableData.length; i++) {
+            tableHtml += '<tr>';
+            for (var j = 0; j < tableData[i].length; j++) {
+                if (i === 0) {
+                    tableHtml += '<th>' + tableData[i][j] + '</th>';
+                } else {
+                    tableHtml += '<td>' + tableData[i][j] + '</td>';
+                }
+            }
+            tableHtml += '</tr>';
+        }
+
+        document.getElementById('tabla-diccionario').innerHTML = tableHtml;
+    }
+});
